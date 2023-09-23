ad8c9f0210bcbd3080a5edb68e6d69894327ba9a
OjoLego <legoviewsphotography@gmail.com>
2023-09-23 08:00:07 +0100

    News App

--- app/src/main/java/com/loc/newsapp/presentation/navgraph/NavGraph.kt
+++ app/src/main/java/com/loc/newsapp/presentation/navgraph/NavGraph.kt
@@ -7,6 +7,9 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navigation
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.loc.newsapp.presentation.home.HomeScreen
+import com.loc.newsapp.presentation.home.HomeViewModel
 import com.loc.newsapp.presentation.onboarding.OnBoardingScreen
 import com.loc.newsapp.presentation.onboarding.OnBoardingViewModel
 
@@ -43,7 +46,9 @@ fun NavGraph(
             composable(
                 route = Route.NewsNavigationScreen.route
             ){
-                Text(text = "News Navigator Screen")
+                val viewModel: HomeViewModel = hiltViewModel()
+                val article = viewModel.news.collectAsLazyPagingItems()
+                HomeScreen(articles = article, navigate = {})
             }
         }
     }
